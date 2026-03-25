3ee7ee690df023e5007423efe92ba102a9b07571
mateuseduardo250 <mateuseduardo250@gmail.com>
2026-03-25 20:04:42 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,19 @@ export default function HomePage() {
   return (
     <main>
       <Header />
-      <div style={{
-  marginTop: 12,
-  marginBottom: 12,
-  padding: 12,
-  background: '#d1fae5',
-  borderRadius: 12,
-  fontWeight: 600,
-  textAlign: 'center',
-  color: '#065f46'
-}}>
+<div
+  style={{
+    marginTop: 12,
+    marginBottom: 12,
+    padding: 12,
+    background: 'linear-gradient(90deg, #065f46, #10b981)',
+    borderRadius: 12,
+    fontWeight: 600,
+    textAlign: 'center',
+    color: 'white',
+    boxShadow: '0 10px 24px rgba(16, 185, 129, 0.22)',
+  }}
+>
   Categorias
 </div>
       <ProductList />
