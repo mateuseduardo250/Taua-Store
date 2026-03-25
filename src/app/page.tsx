0c2583e0784765e0454bdc5244c7e63186c307b5
mateuseduardo250 <mateuseduardo250@gmail.com>
2026-03-25 16:29:11 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,12 @@
 import { Header } from '@/components/layout/Header';
 import { ProductList } from '@/components/products/ProductList';
-import { CartSummary } from '@/components/cart/CartSummary';
 import { Footer } from '@/components/layout/Footer';
 
 export default function HomePage() {
   return (
     <main>
       <Header />
-
-      {/* PRODUTOS */}
       <ProductList />
-
-      {/* CARRINHO EMBAIXO */}
-      <div style={{ marginTop: 24 }}>
-        <CartSummary />
-      </div>
-
       <Footer />
     </main>
   );
